dfdf84e6bbfe786f0a36500ea388b89cc5c15374
Jessica Kennair <jken079@gmail.com>
2014-10-30 16:49:30 +1300

    have created models for the db which are really just classes for entity code first, will keep on this track as following ASP.NET MVC framework

--- ResilientOrg/ResillientOrg/Models/Message.cs
+++ ResilientOrg/ResillientOrg/Models/Message.cs
@@ -7,5 +7,11 @@ namespace ResilientOrg.Models
 {
     class Message
     {
+        public int MessageId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
     }
 }
--- ResilientOrg/ResillientOrg/Models/Post.cs
+++ ResilientOrg/ResillientOrg/Models/Post.cs
@@ -7,5 +7,14 @@ namespace ResilientOrg.Models
 {
     class Post
     {
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int UserId { get; set; }
+            public virtual User User { get; set; }
+        }
     }
 }
